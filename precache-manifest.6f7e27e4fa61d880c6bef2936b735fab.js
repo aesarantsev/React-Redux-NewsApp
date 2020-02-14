0560aeed8da89883d345dd09455c7e926db87a29
aesarantsev <andrrr96@yandex.ru>
2020-02-14 12:54:55 +1000

    Updates

---
+++ precache-manifest.6f7e27e4fa61d880c6bef2936b735fab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd54f0a16cb2dd92c32345216ae0439d",
+    "url": "/React-Redux-NewsApp/index.html"
+  },
+  {
+    "revision": "c5f4135fd7619f505d11",
+    "url": "/React-Redux-NewsApp/static/css/2.7efaf445.chunk.css"
+  },
+  {
+    "revision": "c5f4135fd7619f505d11",
+    "url": "/React-Redux-NewsApp/static/js/2.b8b0ca31.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/React-Redux-NewsApp/static/js/2.b8b0ca31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35d0b313f95192e5c1ed",
+    "url": "/React-Redux-NewsApp/static/js/main.b74c1c7a.chunk.js"
+  },
+  {
+    "revision": "3d95133a4d13b32fc95c",
+    "url": "/React-Redux-NewsApp/static/js/runtime-main.bb7ddecc.js"
+  },
+  {
+    "revision": "3b84cf99bdb036605d5ff8f60826647c",
+    "url": "/React-Redux-NewsApp/static/media/spinner.3b84cf99.svg"
+  }
+]);
